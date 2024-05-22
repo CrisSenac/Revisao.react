4c5baee8ab8b65d5a6cf16445c814e6685901c5f
CrisSenac <crislene.ssilva1@senacsp.edu.br>
2024-05-22 17:17:42 -0300

    Header React

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import Logo from './components/Logo';
 import './App.css';
 
+import OptionsHeader from './components/OptionsHeader';
+import IconesHeader from './components/IconesHeader';
+import Header from './components/Header';
+
 function App() {
   return (
     <div className="App">
     <header className="App-header">
       <Logo /> 
-
+      <OptionsHeader />
+      <IconesHeader />
+      <Header />
       </header>
       </div>
   );
---
+++ src/components/Header/index.js
@@ -0,0 +1,7 @@
+import './index.js';
+
+<header className="App-header">
+<Logo /> 
+<OptionsHeader />
+<IconesHeader />
+<Header>
