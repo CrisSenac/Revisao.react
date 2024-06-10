d65cd6a191d43da8a5f64cce3cfecb5669f4e1ee
CrisSenac <crislene.ssilva1@senacsp.edu.br>
2024-06-10 14:50:02 -0300

    aula 10-06

--- src/components/Pesquisa/index.js
+++ src/components/Pesquisa/index.js
@@ -1,5 +1,7 @@
 import Input from "../Input";
 import styled from "styled-components";
+import { useState } from "react";
+import {livros} from '../Pesquisa/dadosPesquisa';
 
 const PesquisaContainer= styled.section`
     background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -22,15 +24,20 @@ const Subtitulo = styled.h3`
 `
 
 function Pesquisa () {
+    const [livrosPesquisados, setLivrosPesquisados] = useState([])
+    console.log(livrosPesquisados)
     return(
         <PesquisaContainer>
             <Titulo>Já sabe por onde começar?</Titulo>
             <Subtitulo>Encontre o seu livro em nossa estante</Subtitulo>
             <Input placeholder="Escreva sua próxima leitura" 
-            onBlur={evento => console.log (evento.target.value)}
-            
-            />
-
+            onBlur={evento => { 
+        const textoDigitado = evento.target.value
+        const resultadoPesquisa = livros.filter(livro => livro.nome.
+            includes(textoDigitado))
+            setLivrosPesquisados(resultadoPesquisa)
+    } }
+               />
         </PesquisaContainer>
     )
 }
